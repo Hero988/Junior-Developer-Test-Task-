af65d33e150875bb003ad8f58a833981a7b0bccb
Suliman <gissuliman@gmail.com>
2023-05-06 17:43:35 +0100

    work4

--- src/App.js
+++ src/App.js
@@ -75,19 +75,19 @@ function App() {
                                     <div key={product.id} className="product-box">
                                         <input
                                             type="checkbox"
-                                            className=".delete-checkbox, delete-checkbox"
+                                            className=".delete-checkbox"
                                             checked={selectedProducts.includes(product.id)}
                                             onChange={() => toggleProductSelection(product.id)}
                                             style={{ display: products.length === 0 ? "none" : "inline" }}
                                         />
-                                        <h2>{product.sku}</h2>
+                                        <h2>className=".delete-checkbox"{product.sku}</h2>
                                         <p2>{product.name}</p2>
                                         <p2>${product.price}</p2>
                                         <p2>{product.attribute}</p2>
                                     </div>
                                 ))
                             ) : (
-                               <p className="delete-checkbox"></p>
+                               <p className="work :)"></p>
                             )}
                         </div>
                     }
